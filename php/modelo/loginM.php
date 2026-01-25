b467826ff7bcf14cbfeece5ceb75de86a4b3f964
javier farinango <javier.farinango92@gmail.com>
2026-01-25 03:18:27 -0500

    comit

--- php/modelo/loginM.php
+++ php/modelo/loginM.php
@@ -78,11 +78,11 @@ class loginM
     {
     	$id_empresa = $_SESSION['INICIO']['ID_EMPRESA'];
     	$sql = "UPDATE  notificaciones
-    			SET leido = '1' 
+    			SET leido = 1 
     			WHERE id_noti='".$id."'"; 
 
     	// print_r($sql);die();
-    	$result = $this->db->datos($sql,$id_empresa);
+    	$result = $this->db->sql_string($sql,$id_empresa);
         return $result;
 
     }
